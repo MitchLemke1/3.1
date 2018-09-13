1bbd03a5a5f6902423a3357dfdac8abca5eee69d
MitchLemke1 <42777506+MitchLemke1@users.noreply.github.com>
2018-09-13 09:49:20 -0500

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,18 @@
+user = input("Choose a number")
+
+
+
+if user == 1:
+    print("Monday")
+elif user == 2:
+    print("Tuesday")
+elif user == 3:
+    print("wednesday")
+elif user == 4:
+    print("Thursday")
+elif user == 5:
+    print("Friday")
+else:
+    print("not a day of the week")
+
+user = print("today is 3")
